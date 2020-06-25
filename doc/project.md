45b0b26640f0eb7ee0f04bc2ee702124dd409406
Malte Kießling <mkalte@mkalte.me>
2020-06-25 17:01:17 +0200

    project template

---
+++ doc/project.md
@@ -0,0 +1,5 @@
+# Template Project {#mainpage}
+
+Welcome to the documentation of the template project. 
+
+Use at your own risk. Have fun. Stay safe. 
---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+/*
+ * This file is part of a Template Project
+ * Copyright (c) 2020 Malte Kießling
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * \file main.cpp
+ * \brief Some Source File
+ */
+
+int someFunction(int param)
+{
+    return param * param;
+}
+
+int main(int, char**)
+{
+    return 0;
+}
